d0e86671312bd14ad58448bc21794d104aef317f
Muhammad Nuzaihan <muhammad@taqisystems.com>
2016-10-06 15:40:43 +0800

    fix nil

--- lib/docsplit_images/docsplit_images_job.rb
+++ lib/docsplit_images/docsplit_images_job.rb
@@ -5,10 +5,8 @@ class DocsplitImages::DocsplitImagesJob
   include ::Sidekiq::Worker
 
   def perform(class_name, id)
-    object = class_name.constantize.find_by(id: id)
+    object = Document.find(id)
     object.docsplit_images_process
-    logger.debug "id: #{id}"
-    logger.debug "class_name: #{class_name}"
   end
 
 end
