8164544fd4c2d82dd0daf86a106653b8beda7ddc
Muhammad Nuzaihan <muhammad@taqisystems.com>
2016-10-06 15:36:42 +0800

    set up logger

--- lib/docsplit_images/docsplit_images_job.rb
+++ lib/docsplit_images/docsplit_images_job.rb
@@ -6,9 +6,9 @@ class DocsplitImages::DocsplitImagesJob
 
   def perform(class_name, id)
     object = class_name.constantize.find_by(id: id)
-    object.docsplit_images_process 
-    puts class_name
-    puts id   
+    object.docsplit_images_process
+    logger.debug "id: #{id}"
+    logger.debug "class_name: #{class_name}"
   end
 
 end
