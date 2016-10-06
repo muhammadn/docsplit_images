ed540a0e4267ca5a6ed4a0b987ef3338a26c4c7a
Muhammad Nuzaihan <muhammad@taqisystems.com>
2016-10-06 16:09:56 +0800

    revert back some changes

--- lib/docsplit_images/docsplit_images_job.rb
+++ lib/docsplit_images/docsplit_images_job.rb
@@ -1,11 +1,10 @@
 require 'sidekiq'
-require 'docsplit_images/conversion'
 
 class DocsplitImages::DocsplitImagesJob
   include ::Sidekiq::Worker
 
   def perform(class_name, id)
-    object = Document.find_by(id: 32)
+    object = class_name.constantize.find_by(id: 32)
     object.docsplit_images_process
   end
 
