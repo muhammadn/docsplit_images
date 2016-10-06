4905156a33339d65d15a50a836502ab23d3fc14f
Muhammad Nuzaihan <muhammad@taqisystems.com>
2016-10-06 15:21:14 +0800

    find_by_ is deprecated

--- lib/docsplit_images/docsplit_images_job.rb
+++ lib/docsplit_images/docsplit_images_job.rb
@@ -5,7 +5,7 @@ class DocsplitImages::DocsplitImagesJob
   include ::Sidekiq::Worker
 
   def perform(class_name, id)
-    object = class_name.constantize.find_by_id(id)
+    object = class_name.constantize.find_by(id: id)
     object.docsplit_images_process    
   end
 end
